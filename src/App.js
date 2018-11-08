821487dfb7db36ff07866e8332d8f247689c10bf
Harry Luo <hluo@konradgroup.com>
2018-11-08 18:44:35 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
-import React, { Component } from 'react';
+import React, { Fragment, Component } from 'react';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Grid from '@material-ui/core/Grid';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Fragment>
+        <CssBaseline />
+        <div className="App">
+          <Grid container spacing={16} justify="center">
+            <Grid item xs={12}>
+              TEST
+            </Grid>
+          </Grid>
+        </div>
+      </Fragment>
     );
   }
 }
